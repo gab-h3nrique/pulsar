4718adf2d8426305b3ebb61442f2e3dc39eafdf1
gab Henrique <gabrielbielrique10@gmail.com>
2025-05-31 15:28:03 -0300

    fixing some issues on docker network creation

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/pulsar',
     output: "standalone",
     env: { SOCKET_URL: process.env.SOCKET_URL, BASE_API_URL: process.env.BASE_API_URL },
     reactStrictMode: false
